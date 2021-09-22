45e5ac55c1749a04906c9233b55d2b36b6d2eb52
Christopher Nellis <christopher.t.nellis@gmail.com>
2021-09-21 23:38:45 -0400

    Migrate to Jackson

--- src/main/java/io/nellis/MainFunction.java
+++ src/main/java/io/nellis/MainFunction.java
@@ -3,8 +3,7 @@ package io.nellis;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.nellis.model.JokeResponse;
 import io.nellis.model.ServerResponse;
 import lombok.SneakyThrows;
@@ -16,7 +15,7 @@ import java.net.http.HttpResponse;
 import java.util.Map;
 
 public class MainFunction implements RequestHandler<Map<String, String>, JokeResponse> {
-    Gson gson = new GsonBuilder().setPrettyPrinting().create();
+    ObjectMapper mapper = new ObjectMapper();
 
     @SneakyThrows
     @Override
@@ -33,7 +32,7 @@ public class MainFunction implements RequestHandler<Map<String, String>, JokeRes
         HttpResponse<String> httpResponse =
                 client.send(request, HttpResponse.BodyHandlers.ofString());
 
-        ServerResponse decodedResponse = gson.fromJson(httpResponse.body(), ServerResponse.class);
+        ServerResponse decodedResponse = mapper.readValue(httpResponse.body(), ServerResponse.class);
 
         return new JokeResponse(decodedResponse.getSetup(), decodedResponse.getDelivery());
     }
