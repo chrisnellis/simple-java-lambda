ca30c816269e387b28f4d0c49e9ff9bd131ecc10
Christopher Nellis <christopher.t.nellis@gmail.com>
2021-09-21 22:16:19 -0400

    Improve deployment file

--- deployToAWS.sh
+++ deployToAWS.sh
@@ -1,3 +1,3 @@
 ./gradlew buildZip
 aws lambda update-function-code --function-name javaFunction --zip-file fileb://build/distributions/basicJoker-1.0-SNAPSHOT.zip > /dev/null
-exit
+aws lambda update-function-configuration --function-name javaFunction --handler io.nellis.MainFunction > /dev/null
